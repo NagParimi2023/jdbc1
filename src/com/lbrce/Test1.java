48d30d7929f06b1d9bd94b77daf4527c21d1abc6
Admin <Admin@DESKTOP-MM7757G>
2023-10-18 18:46:15 +0530

    commit

---
+++ src/com/lbrce/Test1.java
@@ -0,0 +1,16 @@
+package com.lbrce;
+
+import java.util.*;
+
+public class Test1 {
+	public static void main(String[] args) {
+		String str="Nag";
+	     if(str=="Nag") {
+	    	 System.out.println("Successfully Login");
+	    	 hhiiiiiiii
+	     }else {
+	    	 System.out.println("UnSuccessfully");
+	     }
+	}
+     
+}
